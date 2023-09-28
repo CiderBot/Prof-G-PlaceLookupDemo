5c06c243c87abb177f3e971114526268edba0178
Steven Yung <steve@redricebowl.com>
2023-09-28 16:33:04 -0700

    Initial Commit

---
+++ PlaceLookupDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlaceLookupDemo
+//
+//  Created by Steven Yung on 9/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlaceLookupDemo/PlaceLookupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceLookupDemoApp.swift
+//  PlaceLookupDemo
+//
+//  Created by Steven Yung on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct PlaceLookupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
