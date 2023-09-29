dadf0c9b26d102bab0aa4d8934ee40ccf7740eb1
Steven Yung <steve@redricebowl.com>
2023-09-28 23:39:43 -0700

    complete place lookup

--- PlaceLookupDemo/ContentView.swift
+++ PlaceLookupDemo/ContentView.swift
@@ -6,22 +6,41 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     @EnvironmentObject var locationManager: LocationManager
+    @State private var showPlaceLookupSheet = false
+    @State var returnPlace = Place(mapItem: MKMapItem())
     
     var body: some View {
-        VStack (alignment: .leading){
-            Image(systemName: "globe")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.tint)
-            Spacer()
-            Text("Location:\n lat: \(locationManager.location?.coordinate.latitude ?? 0.0)\n long: \(locationManager.location?.coordinate.longitude ?? 0.0)")
-                .font(.title)
-            Spacer()
+        NavigationStack {
+            VStack (alignment: .leading){
+                Image(systemName: "globe")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.tint)
+                Spacer()
+                Text("Location:\n lat: \(locationManager.location?.coordinate.latitude ?? 0.0)\n long: \(locationManager.location?.coordinate.longitude ?? 0.0)")
+                    .font(.title)
+                Text("\nReturned Place\nName: \(returnPlace.name)\nAddress:\(returnPlace.address)\nCoords: \(returnPlace.latitude), \(returnPlace.longitude)")
+                Spacer()
+            }
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button(action: {
+                        showPlaceLookupSheet.toggle()
+                    }, label: {
+                        Image(systemName: "magnifyingglass")
+                        Text("Lookup Place")
+                    })
+                }
+            }
         }
-        .padding()
+        .fullScreenCover(isPresented: $showPlaceLookupSheet, content: {
+            PlaceLookupView(returnedPlace: $returnPlace)
+        })
     }
 }
 
